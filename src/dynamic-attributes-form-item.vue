891152ae1e46075db5542015663e887aeb932af2
shixianwei <shixw_usr@126.com>
2019-11-08 11:29:13 +0800

    项目初始化

---
+++ src/dynamic-attributes-form-item.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  asdasds
+</div>
+</template>
+
+<script>
+    export default {
+        name: "dynamic-attributes-form-item"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import DynamicAttributesFormItem from './dynamic-attributes-form-item';
+export default DynamicAttributesFormItem;
+if (typeof window != 'undefined' && window.Vue){
+  window.Vue.component('dynamic-attributes-form-item',DynamicAttributesFormItem);
+}
